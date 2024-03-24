17e64dfc8b17aabf698dc179f98d733f92dbe8b2
jinho9482 <jinhojo9482@gmail.com>
2024-03-25 00:41:11 +0900

    See how event works

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,26 @@
     <a v-for="(name, id) in menu" :key="id" href=""> {{ name }} </a>
   </div>
 
-  <div v-for="(name, id) in products" :key="id">
-    <h4 :style="style">{{ name }}</h4>
+  <div>
+    <h4 :style="style">{{ products[0] }}</h4>
     <p>{{ price1 }} 만원</p>
     <!-- 데이터 바인딩 : 변경이 용이함, 실시간 자동 렌더링으로 값을 변경하면 그 즉시 웹사이트에 반영됨 -> 자주 변경될 애들을 이렇게 해놓으면 좋다. -->
+    <button @click="claim[0]++">허위 매물 신고</button>
+    <span>신고 수 : {{ claim[0] }} </span>
+  </div>
+
+  <div>
+    <h4 :style="style">{{ products[1] }}</h4>
+    <p>{{ price2 }} 만원</p>
+    <button @click="claim[1]++">허위 매물 신고</button>
+    <span>신고 수 : {{ claim[1] }} </span>
+  </div>
+
+  <div>
+    <h4 :style="style">{{ products[2] }}</h4>
+    <p>{{ price3 }} 만원</p>
+    <button @click="claim[2]++">허위 매물 신고</button>
+    <span>신고 수 : {{ claim[2] }} </span>
   </div>
 </template>
 
@@ -23,8 +39,16 @@ export default {
       style: "color: blue",
       products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
       menu: ["Home", "Shop", "About"],
+      claim: [0, 0, 0],
     };
   },
+
+  // methods: {
+  //   increase() {
+  //     this.claim++;
+  //   },
+  // },
+
   components: {},
 };
 </script>
